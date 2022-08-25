2467afba86e2764d1126cc8e75aaafa5548bac15
jakewilliams33 <jakewilliams33@hotmail.com>
2022-08-25 09:34:07 +0100

    add sorting to articles

--- src/Components/ArticlesPage.jsx
+++ src/Components/ArticlesPage.jsx
@@ -1,20 +1,45 @@
 import { useState, useEffect } from "react";
 import { getArticles } from "../api";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import Select from "react-select";
 
 export const ArticlesPage = () => {
   const [articles, setArticles] = useState([]);
+  const [queryStr, setQueryStr] = useState();
   const { topic } = useParams();
-  console.log(topic);
+
+  let placeholder = "";
+
+  if (typeof topic === "string") {
+    placeholder = topic + "/";
+  }
+
+  const navigate = useNavigate();
+
+  const handleQuery = (event) => {
+    setQueryStr(`sort_by=${event.value}`);
+    navigate(`/${placeholder}${event.url}`, { replace: true });
+  };
 
   useEffect(() => {
-    getArticles(topic).then((articleList) => {
+    getArticles(topic, queryStr).then((articleList) => {
+      console.log(queryStr);
       setArticles(articleList.articles);
     });
-  }, [topic]);
+  }, [topic, queryStr]);
+
+  const options = [
+    { url: "sort/1", value: "created_at&order=desc", label: "newest" },
+    { url: "sort/2", value: "created_at&order=asc", label: "oldest" },
+    { url: "sort/3", value: "title&order=asc", label: "title" },
+    { url: "sort/4", value: "topic&order=asc", label: "topic" },
+    { url: "sort/5", value: "votes", label: "votes" },
+    { url: "sort/6", value: "comment_count", label: "comments" },
+  ];
 
   return (
     <section>
+      <Select className="sortBy" options={options} onChange={handleQuery} />
       <hr className="topLine"></hr>
       {articles.map((article) => {
         return (
--- src/api.js
+++ src/api.js
@@ -8,22 +8,17 @@ export const getTopics = () => {
     });
 };
 
-export const getArticles = (topic) => {
-  if (topic) {
-    return axios
-      .get(
-        `https://jakes-nc-news-app.herokuapp.com/api/articles?topic=${topic}`
-      )
-      .then(({ data }) => {
-        return data;
-      });
-  } else {
-    return axios
-      .get(`https://jakes-nc-news-app.herokuapp.com/api/articles`)
-      .then(({ data }) => {
-        return data;
-      });
-  }
+export const getArticles = (topic, queryStr) => {
+  const queryOption = queryStr ? queryStr : "";
+  const topicOption = topic ? `?topic=${topic}` : "";
+  const andOr = topic ? "&" : "?";
+  return axios
+    .get(
+      `https://jakes-nc-news-app.herokuapp.com/api/articles${topicOption}${andOr}${queryOption}`
+    )
+    .then(({ data }) => {
+      return data;
+    });
 };
 
 export const getSingleArticle = (article_id) => {
